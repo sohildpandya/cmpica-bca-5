b392d3dfa3dd4a694996503474ccfb7364d710bf
sohildpandya <sohilpandya@gmail.com>
2021-10-20 11:23:21 +0530

    Create gethint.php

---
+++ ajax/example-1/gethint.php
@@ -0,0 +1,10 @@
+<?php
+	$myGender = $_GET['sohil'];
+
+	if ($myGender=="m") {
+		echo "Welcome <font face='arial' color='red'><em>Sir</em></font>";
+	}
+	else {
+		echo "Welcome <font face='arial' color='blue'><em>Madam</em></font>";
+	}
+?>
