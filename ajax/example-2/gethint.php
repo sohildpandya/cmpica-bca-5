a6a7b9b2bc6b584c544f3cc646f6d22351372f43
sohildpandya <sohilpandya@gmail.com>
2021-10-21 09:52:19 +0530

    Update gethint.php

--- ajax/example-2/gethint.php
+++ ajax/example-2/gethint.php
@@ -1,17 +1,14 @@
 <?php
 // get the q parameter from URL
 $q = $_REQUEST["q"];
-
 $file = fopen("demo.csv","r");
 $j=0;
 $hint="";
 echo "<strong>";
- while (!feof($file)) // Reading up to the end of the file
+while (!feof($file)) // Reading up to the end of the file
 {        
 	$array=fgetcsv($file);	// Getting first row as an array
-
 	$myF=$array[1];
-
 	if ($q !== "") {
 		$q = strtolower($q);
 		$len=strlen($q);
@@ -19,13 +16,9 @@ echo "<strong>";
 			$j++;
 			$hint.="<br>".$array[1]." ".$array[0];
 		}
-		
 	}
 }
-
 echo $hint === "" ? "no suggestion" : $hint;
 echo "<strong>";
 echo "<br><br><font color='red'>".$j." matches found</font>";
-
-
 ?>
